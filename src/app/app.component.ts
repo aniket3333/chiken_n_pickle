bc4e49daf50d2906550bc88aab997bb0b68e5cf4
aniket3333 <anikethiware3@gmail.com>
2024-08-13 19:37:31 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'chiken_n_pickle';
+}
